843f6e7161a5efd4829dcade16fb57986dbf07e1
me001416 <me001416@gmail.com>
2017-02-06 15:50:48 +0800

    Version 1.0.21 Add new struct File_Owner

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/FileControll.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public struct File_Owner
+    {
+        protected string dirPath = @"D:\CODE\ForFun\C_Sharp\Test";
+        protected string txtPath = @"D:\CODE\ForFun\C_Sharp\Test\text1.txt";
+
+        public StreamWriter sw;
+
+        public Boolean FileOrDirectoryExists(string name)
+        {
+            return (Directory.Exists(name) || File.Exists(name));
+        }
+
+        public File_Owner(string FileName, Boolean Active) : this()
+        {
+            String _fileName;
+
+            if (FileName == null || !FileOrDirectoryExists(FileName))
+            {
+                _fileName = txtPath;
+            }
+            else
+            {
+                _fileName = FileName;
+            }
+
+            sw = new StreamWriter(_fileName);
+        }
+    }
+}
