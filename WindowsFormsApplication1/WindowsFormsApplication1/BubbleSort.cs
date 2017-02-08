37fdf06ff10ad5f2a0c0eafb5cbd5e92f48a09d0
me001416 <me001416@gmail.com>
2017-02-08 16:11:04 +0800

    Version 1.0.34 Add BubbleSort.cs

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/BubbleSort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WindowsFormsApplication1
+{
+    public class BubbleSort
+    {
+        public BubbleSort(List<int> x, int count)
+        {
+            if (x.Count != 0)
+            {
+                for (int i = 1; i <= x.Count; i++)
+                {
+                    for (int j = 1; j <= x.Count; j++)
+                    {
+                        if (x[j] < x[j - 1])
+                        {
+                            int temp = x[j];
+                            x[j] = x[j - 1];
+                            x[j - 1] = temp;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
