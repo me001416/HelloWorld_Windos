c78c1c9f166ce5e7d07f842ca85fdaffcc548df1
me001416 <me001416@gmail.com>
2017-04-17 14:38:46 +0800

    Version 1.0.75
    1. Implement class RandomNum

--- WindowsFormsApplication1/WindowsFormsApplication1/RandomNumber.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/RandomNumber.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public class RandomNum
+    {
+        Random mRandom;
+
+        public RandomNum()
+        {
+            Random mRandom = new Random(Guid.NewGuid().GetHashCode());
+        }
+
+        public List<int> GetRandomNum()
+        {
+            List<int> NumList = new List<int>();
+
+            for (int i = 0; i < 6; i++)
+            {
+                NumList.Add(mRandom.Next(1, 50));
+            }
+
+            return NumList;
+        }
+    }
+}
